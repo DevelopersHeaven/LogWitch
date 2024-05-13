DevelopersHeaven/LogWitch

commit e261d7bd1fc01498f5eaa44a347b455531acab4e

Cleanup: Replace `static_cast` with `dynamic_cast`
Suggested by clang-tidy.

logWitch/GUITools/SynchronizedHeaderView.cpp
@@ -85,7 +85,7 @@ void HeaderViewGroup::synchronizeState( SynchronizedHeaderView *view )
 
 void HeaderViewGroup::headerChildDestroyed( QObject *obj )
 {
-    auto *view = static_cast<SynchronizedHeaderView *>(obj);
+    auto *view = dynamic_cast<SynchronizedHeaderView *>(obj);
 
     auto it = std::find( m_groupChilds.begin(), m_groupChilds.end(), view );
     if( it != m_groupChilds.end() )

logWitch/GUITools/EventFilterToBoostFunction.cpp
@@ -15,7 +15,7 @@ namespace evtFunc
     {
         if (e->type() == QEvent::KeyPress )
         {
-             auto *keyEvent = static_cast<QKeyEvent *>(e);
+             auto *keyEvent = dynamic_cast<QKeyEvent *>(e);
              if( keyEvent->key() == keycode )
              {
                  f();

commit 9b8e02187ae02b8690abd2d9a9485f0332babfa7

Fix segmentation fault when closing a log window, then opening another
This bug was introduced by commit
e261d7bd1fc01498f5eaa44a347b455531acab4e, which changed a `static_cast`
to a `dynamic_cast`.

As it turns out, one cannot `dynamic_cast` an object that is in the
process of being destroyed - with control moving up the chain of virtual
destructors - to a subclass whose destructor has already been invoked.
As a result, `view` equals `nullptr` and the child object is not purged
from the group.

To fix this bug, remove the harmful `dynamic_cast`; `std::find()` works
without a cast.

logWitch/GUITools/SynchronizedHeaderView.cpp
@@ -78,9 +78,7 @@ void HeaderViewGroup::synchronizeState( SynchronizedHeaderView *view )
 
 void HeaderViewGroup::headerChildDestroyed( QObject *obj )
 {
-    auto *view = dynamic_cast<SynchronizedHeaderView *>(obj);
-
-    auto it = std::find( m_groupChilds.begin(), m_groupChilds.end(), view );
+    auto it = std::find(m_groupChilds.begin(), m_groupChilds.end(), obj);
     if( it != m_groupChilds.end() )
         m_groupChilds.erase( it );
     else